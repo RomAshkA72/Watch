dafc6ac4cc49aa3f36efd28ac794f9ea09f74292
Roman <dervoedroman05@gmail.com>
2024-11-05 20:24:01 +0300

    third commit

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 let timerId; // переменная, которая будет хранить ID таймера
 
-
 const startButton = document.getElementById('start');
 startButton.addEventListener('click', function () {
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
     timerId = setInterval(updateClock, 1); // запускаем  updateClock() каждую секунду
 });
 
 
+
 const stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', function () {
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
-    clearInterval(timerId); // останавливаем таймер
+    clearInterval(timerId);
 });
 
 
 function updateClock() {
     const clock = document.getElementById('clock');
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
-    clock.textContent = hours + ':' + minutes + ':' + seconds;
+    let result = [hours, minutes, seconds].map((item) => {
+        return addZero(item);
+    });
+    clock.textContent = result[0] + ':' + result[1]+ ':' + result[2];
+}
+
+function addZero(numder){
+    if (String(numder).length === 1) {
+        return `0${numder}`
+    } else {
+        return String(numder);
+    }
 }
